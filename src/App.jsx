4f75897627d22fc4c00ac53e4209cc2d7bf2f87b
WelderBm <welderb40@gmail.com>
2022-07-30 13:50:46 -0400

    Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { Post } from "./post"
-import "./global.css"
-import { Header } from "./components/Header" 
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./post";
+import "./global.css";
+
+import styles from "./App.module.css";
 
 export const App = () => {
   return (
     <div>
-      <Header/>
-      <Post 
-        author="Welder"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste culpa ea asperiores tempore ullam rem vero omnis totam sequi placeat quas tenetur nihil a non voluptatum similique ab, magnam itaque officia numquam at. Sit, tempora ullam est nemo distinctio pariatur doloremque expedita, eos inventore quibusdam velit, minus nihil. Dolore, voluptates!"
-      />
-      <Post 
-        author="Diego"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste culpa ea asperiores tempore ullam rem vero omnis totam sequi placeat quas tenetur nihil a non"
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
     </div>
-  )}
+  );
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import style from "./Header.module.css"
 
-import ignitLogo from "../assets/ign=nite-logo.svg"
+import ignitLogo from "../assets/ignite-logo.svg"
 
 export const Header = () => {
     return (
         <header className={style.header}>
-            <img src={igniteLogo} alt="logo ignite" />
+            <img src={ignitLogo} alt="logo ignite" />
         </header>
     )
 }
